2f8f475140b68a6629a8f0501dc03d5f2870648a
root <root@DS8614405.clientshostname.com>
2019-05-05 00:19:20 +0200

    added provider:script2

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_ID" --password-stdin
-docker push swording/multi-client
-docker push swording/multi-nginx
-docker push swording/multi-server
-docker push swording/multi-worker
